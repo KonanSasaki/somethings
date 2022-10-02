f7fd4bfc5ab3ee0848a3f7feea42493ce0efbeb9
KonanSasaki <32872889+KonanSasaki@users.noreply.github.com>
2022-10-02 22:47:12 +0900

    Update 画像集め.py

--- 画像集め.py
+++ 画像集め.py
@@ -1,9 +1,8 @@
-# ライブラリを呼び出す
-##lxmlのバージョンは3.7.3以下にする。そうじゃないとエラーが発生する。
+#lxmlのバージョンは3.7.3以下にする。そうじゃないとエラーが発生する。
 from icrawler.builtin import BingImageCrawler
 
-# クローラーを生成、保存先などを指定（今回は'images'フォルダに指定）
-bing_crawler = BingImageCrawler(downloader_threads=4,storage={'root_dir':'C:\\Users\\konan\\Desktop\\color_cone'})
+# クローラーを生成、保存先などを指定
+bing_crawler = BingImageCrawler(downloader_threads=4,storage={'root_dir':'C:\\XXXX'})
 
 # キーワードや枚数を入力させてそれに応じて画像収集する
-bing_crawler.crawl(keyword=str(input('名前を打ってね！>>>')), filters=None, offset=0, max_num=int(input('何枚集める？>>>')))
+bing_crawler.crawl(keyword=str(input('キーワード')), filters=None, offset=0, max_num=int(input('枚数')))
