41bebf8878705364a717ce08c0976bf3e88abfb4
KonanSasaki <32872889+KonanSasaki@users.noreply.github.com>
2022-10-05 21:50:21 +0900

    Update GPS_fusion.py

--- GPS_fusion.py
+++ GPS_fusion.py
@@ -6,7 +6,7 @@ import pigpio
 from micropyGPS import MicropyGPS
 
 #参考サイト　https://yyousuke.github.io/matplotlib/cartopy.html
-
+#https://www.youtube.com/watch?v=Akg5iG3gInw&list=LL&index=3
     
 
 
@@ -55,15 +55,15 @@ def main():
                         m1 = fig.add_subplot(111, projection=request.crs) #画面の領域が描写される　111の意味は、1行目1列の1番目という意味　参考サイト　https://qiita.com/kenichiro_nishioka/items/8e307e164a4e0a279734
 
                         # Set map extent to +- 0.01º of the received position
-                        m1.set_extent([my_gps.longitude[0] + 0.01, my_gps.longitude[0] - 0.01, my_gps.latitude[0] + 0.01, my_gps.latitude[0] - 0.01]) #set_extent([西端の経度, 東端の経度, 南端の緯度, 北端の緯度 
+                        m1.set_extent([my_gps.longitude[0] + 0.01, my_gps.longitude[0] - 0.01, my_gps.latitude[0] + 0.01, my_gps.latitude[0] - 0.01]) #set_extent([西端の経度, 東端の経度, 南端の緯度, 北端の緯度 0.01のところを変えると描写されるマップの広さを変えれる
 
                         # Get image at desired zoom
-                        m1.add_image(request, 14) ##画像を指定するセルにはる　ここだとグーグルマップのタイルを貼っている
+                        m1.add_image(request, 14) ##画像を指定するセルにはる　ここだとグーグルマップのタイルを貼っている　右の値はマップの拡大度合い
                         # Print data on console
                         print('=' * 20)
                         print(my_gps.date_string(), tm[0], tm[1], int(tm[2]))
                         print("latitude:", my_gps.latitude[0], ", longitude:", my_gps.longitude[0])    
-                        time.sleep(1)#一秒停止
+                        #time.sleep(1)#一秒停止
     
     # Plot the new position
                         m1.plot(my_gps.longitude[0], my_gps.latitude[0], '.', transform=ccrs.Geodetic())
